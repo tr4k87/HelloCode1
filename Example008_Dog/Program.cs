d4445fa30f6c34fa71d69278542e6872b4e8700b
Sergey <tr4k@mail.ru>
2022-08-05 00:25:37 +0300

    add code examle008

--- Example008_Dog/Program.cs
+++ Example008_Dog/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int count = 0;
+int distance = 10;
+int FirstFriendSpeed = 1;
+int SecondFriendSpeed = 2;
+int DogSpeed = 3;
+int Friend = 2;
+int Time = 0;
+
+while (distance > 1)
+{   
+    if (Friend == 1)
+    {
+        Time = distance/(FirstFriendSpeed+DogSpeed);
+    
+    Friend = 2;
+    }
+
+    else
+    {
+        Time=distance/(SecondFriendSpeed+DogSpeed);
+
+    Friend = 1;
+    }
+distance = distance - (FirstFriendSpeed+SecondFriendSpeed) * Time;
+
+count = count + 1;
+}
+
+Console.Write("Собака пробежит ");
+
+Console.Write(count);
+
+Console.WriteLine(" раз");
